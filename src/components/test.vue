5134951d55441a75d60adc4816c0d196e2550327
linzhengGithub <alidlg9045@126.com>
2020-10-21 18:32:50 +0800

    初始化路由

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,13 +1,13 @@
 <template>
-  <div></div>
+<div>test</div>
 </template>
 
 <script>
-  export default {
-    name: 'test.vue'
-  }
+export default {
+    name: 'test'
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory,createRouter} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue'
 
-const history = createWebHashHistory()
-const route
+const history = createWebHashHistory() //创建模式
+const router = createRouter({ //创建路由
+  history : history,
+  routes: [
+    {
+      path:'/',component: HelloWorld
+    },
+    {
+      path:'/xxx',component: Test
+    }
+  ]
+})
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')

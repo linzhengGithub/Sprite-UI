aa81b9d84366a7db50775c0353ca57f364253a68
linzhengGithub <alidlg9045@126.com>
2020-10-22 16:22:41 +0800

    封装Topnav组件，及其样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,21 @@
 <template>
-<div>
-    <div class="topnav">
-        <div class="logo"></div>
-        <div class="menu"></div>
+    <div>
+        <Topnav/>
+        <div class="banner">
+            <h1>Sprite UI</h1>
+            <h2>一个简单实用的 UI 框架</h2>
+            <p class="actions">
+                <a href="">Github</a>
+                <a href="">开始</a>
+            </p>
+        </div>
     </div>
-    <div class="banner">
-        <h1>Sprite UI</h1>
-        <h2>一个简单实用的 UI 框架</h2>
-        <p class="actions">
-            <a href="">Github</a>
-            <a href="">开始</a>
-        </p>
-    </div>
-</div>
 </template>
 
-<script>
+<script lang="ts">
+    import Topnav from '../components/Topnav.vue';
 export default {
-
+    components: {Topnav}
 }
 </script>
 

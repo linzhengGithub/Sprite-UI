a78bdf155f636ce938b8647de2ad7ac2a4ef978d
linzhengGithub <alidlg9045@126.com>
2020-10-27 17:32:35 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>dialog</div>
+  <div>
+    <Button>Dialog</Button>
+    <Dialog></Dialog>
+  </div>
 </template>
 
 <script>
+  import Dialog from '../lib/Dialog.vue'
+  import Button from '../lib/Button.vue'
   export default {
-    name: 'DialogDemo'
+    name: 'DialogDemo',
+    components: {Button, Dialog}
   }
 </script>
 

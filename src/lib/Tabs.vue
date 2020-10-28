255943d2d97232fc7d345af1b4642ac804fd9b47
linzhengGithub <alidlg9045@126.com>
2020-10-28 15:08:58 +0800

    完成 Tabs nav的下划线

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="sprite-tabs">
     <div class="sprite-tabs-nav">
-      <div class="sprite-tabs-nav-item" v-for="(t,index) in title" :key="index" :class="{selected: t === selected}" @click="select(t)">{{t}}</div>
+      <div class="sprite-tabs-nav-item" v-for="(t,index) in title" :key="index" :class="{selected: t === selected}" @click="select(t)" :ref="el => {if(el) navItems[index] = el}">{{t}}</div>
+      <div class="sprite-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="sprite-tabs-content">
       <component class="sprite-tabs-content-item" :is="current" :key="current.props.title"></component>
@@ -10,7 +11,7 @@
 </template>
 
 <script>
-  import {computed} from 'vue'
+  import {computed,ref,onMounted} from 'vue'
   import Tab from './Tab.vue'
   export default {
     name: 'Tabs',
@@ -20,6 +21,14 @@
       }
     },
     setup(props,context){
+      const navItems = ref([])
+      const indicator = ref(null)
+      onMounted(()=>{
+        const divs = navItems.value
+        const result = divs.find(div => div.classList.contains('selected'))
+        const {width} = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+      })
       const defaults = context.slots.default()
       defaults.forEach((tag)=>{
         if(tag.type !== Tab){
@@ -35,7 +44,7 @@
       const select = (title) => {
         context.emit('update:selected',title)
       }
-      return{defaults,title,current,select}
+      return{defaults,title,current,select,navItems,indicator}
     }
   }
 </script>
@@ -49,6 +58,7 @@
       display: flex;
       color: $color;
       border-bottom: 1px solid $border-color;
+      position: relative;
       &-item {
         padding: 8px 0;
         margin: 0 16px;
@@ -59,6 +69,14 @@
         &.selected {
           color: $blue;
         }
+      }
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
       }
     }
     &-content {

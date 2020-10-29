eb9d5062fee0c1c38cf305b3073c4461116a0591
linzhengGithub <alidlg9045@126.com>
2020-10-29 20:52:06 +0800

    创建Markdown组件，减少代码量

---
+++ src/components/Markdown.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="markdown-body" v-html="content"></article>
+</template>
+
+<script lang="ts">
+  import {ref} from 'vue'
+  export default {
+    name: 'Markdown',
+    props:{
+      path:{
+        type:String,
+        require:true
+      }
+    },
+    setup(props){
+      const content = ref<String>(null)
+      import(props.path).then((result)=>{
+        content.value = result.default
+      })
+      return{content}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

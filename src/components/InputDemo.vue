1998f99ea31e68c2844245fc904da5c256be90f8
linzhengGithub <alidlg9045@126.com>
2020-11-02 14:38:13 +0800

    完成 Input css样式 支持disabled readonly 属性

--- src/components/InputDemo.vue
+++ src/components/InputDemo.vue
@@ -1,12 +1,14 @@
 <template>
-  <Input></Input>
+  <Input value=""/>
+  <Input value="禁用" disabled/>
+  <Input value="只读" readonly/>
 </template>
 
 <script>
   import Input from '../lib/Input.vue'
   export default {
     name: 'InputDemo',
-    components:{Input}
+    components:{Input},
   }
 </script>
 
--- src/lib/Input.vue
+++ src/lib/Input.vue
@@ -1,13 +1,49 @@
 <template>
-  <input type="text"/>
+  <div class="wrapper">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly"/>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'Input'
+    name: 'Input',
+    props:{
+      value:{
+        type:String
+      },
+      disabled:{
+        type:Boolean,
+        default:false
+      },
+      readonly:{
+        type:Boolean,
+        default: false
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
-
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $red: #F1453D;
+  .wrapper {
+    font-size: 14px;display: inline-flex;align-items: center;
+    > input {
+      height: 32px;border: 1px solid $border-color;border-radius: 4px;
+      padding: 0 8px;font-size: inherit;
+      &:hover {border-color: $border-color-hover;}
+      &:focus {
+        box-shadow: inset 0 1px 3px $box-shadow-color; //内阴影，Y轴1px 扩散半径3px
+        outline: none; //取消默认光标点击聚焦
+      }
+      &[disabled], &[readonly] {
+        border-color: #bbb;color: #bbb;cursor: not-allowed;
+      }
+    }
+  }
 </style>

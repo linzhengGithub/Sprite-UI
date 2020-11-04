4b0b9866719f6c58d74780cacaa1dca5e0cf5275
linzhengGithub <alidlg9045@126.com>
2020-11-04 21:43:06 +0800

    实现 Popover 组件点击出现内容

--- src/components/PopoverDemo.vue
+++ src/components/PopoverDemo.vue
@@ -1,12 +1,18 @@
 <template>
-  <Popover></Popover>
+  <Popover>
+    <template v-slot:content>
+      Popover内容
+    </template>
+    <Button>点我</Button>
+  </Popover>
 </template>
 
 <script lang="ts">
   import Popover from '../lib/Popover.vue';
+  import Button from '../lib/Button.vue';
   export default {
     name: 'PopoverDemo',
-    components: {Popover}
+    components: {Popover,Button}
   };
 </script>
 
--- src/lib/Popover.vue
+++ src/lib/Popover.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    Popover
+  <div class="sprite-popover" @click="xxx">
+    <div class="sprite-popover-content"  v-if="visible">
+      <slot name="content"></slot>
+    </div>
+    <span>
+      <slot></slot>
+    </span>
   </div>
 </template>
 
 <script lang="ts">
+  import {ref} from 'vue'
   export default {
-    name: 'Popover'
+    name: 'Popover',
+    setup(){
+      const visible = ref(false)
+      const xxx = () => {
+        visible.value = !visible.value
+      }
+      return {visible,xxx}
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-
+  .sprite-popover{
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+    margin-top: 100px;
+    &-content{
+      position: absolute;
+      bottom: 100%;
+    }
+  }
 </style>

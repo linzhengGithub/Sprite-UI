75167debe7f06322130b19a5800da810997fdfc4
linzhengGithub <alidlg9045@126.com>
2020-11-05 15:55:17 +0800

    解决 关闭重复，收拢close避免忘了取消监听

--- src/lib/Popover.vue
+++ src/lib/Popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sprite-popover" @click="xxx">
+  <div class="sprite-popover" @click="onClick">
     <div class="sprite-popover-content" ref="popoverContent" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -10,47 +10,61 @@
 </template>
 
 <script lang="ts">
-  import {ref} from 'vue'
+  import {ref} from 'vue';
+
   export default {
     name: 'Popover',
-    setup(){
-      const visible = ref<Boolean>(false)
-      const popoverContent = ref<HTMLDivElement>(null)
-      const popoverBtn = ref<HTMLDivElement>(null)
-      const xxx = () => {
-        visible.value = !visible.value
-        if(visible.value === true){
-          setTimeout(()=>{
-            document.body.appendChild(popoverContent.value)
-            let {left,top} = popoverBtn.value.getBoundingClientRect()
-            popoverContent.value.style.left = left + window.scrollX + 'px'
-            popoverContent.value.style.top = top + window.scrollY + 'px'
-            let eventHandler = () => {
-              visible.value = false
-              document.removeEventListener('click',eventHandler)
-            }
-            document.addEventListener('click',eventHandler)
-          })
+    setup() {
+      const visible = ref<Boolean>(false);
+      const popoverContent = ref<HTMLDivElement>(null);
+      const popoverBtn = ref<HTMLDivElement>(null);
+      const positionPopoverContent = () => {
+        document.body.appendChild(popoverContent.value);
+        let {left, top} = popoverBtn.value.getBoundingClientRect();
+        popoverContent.value.style.left = left + window.scrollX + 'px';
+        popoverContent.value.style.top = top + window.scrollY + 'px';
+      };
+      const onClickDocument = (e) => {
+        if (popoverContent.value && (popoverContent.value === e.target || popoverContent.value.contains(e.target))) {
+          return;
         }
+        close()
+      };
+      const open = () => {
+        visible.value = true
+        setTimeout(() => {
+          positionPopoverContent();
+          document.addEventListener('click', onClickDocument);
+        });
+      };
+      const close = () => {
+        visible.value = false;
+        document.removeEventListener('click', onClickDocument);
       }
-      return {visible,xxx,popoverContent,popoverBtn}
+      const onClick = (event) => {
+        if (popoverBtn.value.contains(event.target)) {
+          if (visible.value === true) {
+            close()
+          }else{
+            open()
+          }
+        }
+      };
+      return {visible, onClick, popoverContent, popoverBtn};
     }
   };
 </script>
 
 <style lang="scss" scoped>
-  .sprite-popover{
+  .sprite-popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
     margin-top: 100px;
-    &-content{
+    &-content {
       position: absolute;
       padding-bottom: 11px;
       transform: translateY(-100%);
     }
-  }
-  .sprite-popover-content{
-
   }
 </style>
